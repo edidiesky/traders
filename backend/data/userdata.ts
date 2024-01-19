7861affd0253ed3df9a2623ea3ac78cf9e632c35
edidiesky <essienedidiong1000@gmail.com>
2024-01-19 11:06:32 -0800

    new fixes

--- backend/data/userdata.ts
+++ backend/data/userdata.ts
@@ -3,38 +3,15 @@ import bcrypt from "bcryptjs";
 
 
 const userdata = [
-    // edgar
-    {
-        "username": "DamainMicheal",
-        "fullname": "Damain Micheal",
-        "email": "DamainMicheal103@gmail.com",
-        password: bcrypt.hashSync("12345", 10),
-        "isAdmin": false,
-        "country": 'Germany',
-        "deposit": 2000,
-        "phone": '+ 123 874 3343 ',
-    },
     // Admin
     {
-        "username": "RilwanOgbon",
-        "fullname": "Rilwan Sodik",
-        "email": "roilwanaSodiks@gmail.com",
-        password: bcrypt.hashSync("12345", 10),
+        "username": "AbassAdmin",
+        "fullname": "Abass Admin",
+        "email": "Abass12@gmail.com",
+        password: bcrypt.hashSync("eadb!334nf", 10),
         "isAdmin": true,
         "country": 'France',
         "deposit": 2000,
-        "phone": '+ 123 874 3343 ',
-
-    },
-
-    {
-        "username": "Korobo2833",
-        "fullname": "Micheal Felix",
-        "email": "Korobo2833@gmail.com",
-        password: bcrypt.hashSync("12345", 10),
-        "isAdmin": false,
-        "country": 'Togo',
-        "deposit": 0,
         "phone": '+ 123 874 3343 ',
 
     },
